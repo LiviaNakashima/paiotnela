44fe6dcff6161295fa800dd0f4ec4b481110c104
Aluno Bandtec <github@alunos.bandtec.com.br>
2019-05-17 18:56:54 -0300

    Atualização dados banco-arduino-site

---
+++ Site-Atual/routes/leituras.js
@@ -0,0 +1,73 @@
+// não mexa nestas 3 linhas!
+var express = require('express');
+var router = express.Router();
+var banco = require('../app-banco');
+// não mexa nessas 3 linhas!
+
+router.get('/ultimas', function (req, res, next) {
+  console.log(banco.conexao);
+  banco.conectar().then(() => {
+    var limite_linhas = 3;
+    return banco.sql.query(`select top ${limite_linhas} 
+                            id_evento as id_nome_loko, 
+                            data_hora,
+                            temp_sensor as temp, 
+                            umid_sensor, 
+                            FORMAT(data_hora,'HH:mm:ss') as momento 
+                            from Evento order by id_evento desc`);
+  }).then(consulta => {
+
+    console.log(`Resultado da consulta: ${consulta.recordset}`);
+    res.send(consulta.recordset);
+
+  }).catch(err => {
+
+    var erro = `Erro na leitura dos últimos registros: ${err}`;
+    console.error(erro);
+    res.status(500).send(erro);
+
+  }).finally(() => {
+    banco.sql.close();
+  });
+
+});
+
+
+router.get('/estatisticas', function (req, res, next) {
+  console.log(banco.conexao);
+
+  var estatisticas = {
+    temp_maxima: 0, 
+    temp_minima: 0, 
+    temp_media: 0
+  };
+
+  banco.conectar().then(() => {
+    return banco.sql.query(`
+        select 
+          max(temperatura) as temp_maxima, 
+          min(temperatura) as temp_minima, 
+          avg(temperatura) as temp_media 
+        from leitura
+        `);
+  }).then(consulta => {
+    estatisticas.temp_maxima = consulta.recordset[0].temp_maxima;
+    estatisticas.temp_minima = consulta.recordset[0].temp_minima;
+    estatisticas.temp_media = consulta.recordset[0].temp_media;
+    console.log(`Estatísticas: ${estatisticas}`);
+    res.send(estatisticas);
+  }).catch(err => {
+
+    var erro = `Erro na leitura dos últimos registros: ${err}`;
+    console.error(erro);
+    res.status(500).send(erro);
+
+  }).finally(() => {
+    banco.sql.close();
+  });
+
+});
+
+
+// não mexa nesta linha!
+module.exports = router;
